b233ec13156a89387ebc7d8b1ecb382f13d82804
rootslab <44gatti@gmail.com>
2014-12-26 00:10:18 +0100

    v0.0.35. Better logic/code for SeqTransStream, use items = 0 to filter all data from input stream. On branch master modified:   example/brando-stream-example.js modified:   lib/brando.js modified:   lib/filters/partperm.js modified:   lib/streams/sequence-transform.js modified:   package.json

--- example/brando-stream-example.js
+++ example/brando-stream-example.js
@@ -2,13 +2,13 @@ var log = console.log
     , Brando = require( '../' )
     , fs = require( 'fs' )
     , input = fs.createReadStream( './example/test-data.txt' )
-    , sts = new require( '../lib/streams/sequence-transform' )( 10, 10, {} )
+    , sts = new require( '../lib/streams/sequence-transform' )( 16, 10, {} )
     , dstream = input.pipe( sts )
     , onRead = function () {
         var me = this
             , data = null
             ;
-        while ( data = me.read( 3 ) ) log( 'data:', data );
+        while ( data = me.read(3) ) log( 'data:', data );
     }
     , onEnd = function () {
         var me = this
