908c8b10b84a0c4a7e7ab4f30010abafd8b4019e
rootslab <44gatti@gmail.com>
2014-12-28 00:30:26 +0100

    Added feature to FullPermTransStream, for specifying how many items to emit, to cut internal buffer size, and push out those values that are already in their definitive place, during shuffle. Added comments for FullPerm stream. On branch master modified:   example/fullperm-filter-stream-example.js modified:   lib/filters/streams/fullperm-transform.js

--- example/fullperm-filter-stream-example.js
+++ example/fullperm-filter-stream-example.js
@@ -2,13 +2,21 @@ var log = console.log
     , Brando = require( '../' )
     , fs = require( 'fs' )
     , input = fs.createReadStream( './example/test-data.txt' )
-    , fpts = new require( '../lib/filters/streams/fullperm-transform' )( 2, 7, {} )
+    , fpts = new require( '../lib/filters/streams/fullperm-transform' )( 3, 19, {} )
     , dstream = input.pipe( fpts )
     , onRead = function () {
         var me = this
             , data = null
+            , rbytes = undefined
             ;
-        while ( data = me.read() ) log( 'data:', data );
+        /*
+         * When read_bytes is undefined, it streams 3 items at a time, see constructor,
+         * otherwise it reads rbytes bytes, as usual.
+         * Specifying items, permits to reduce internal Buffer size for the transform
+         * stream; it emits and slices out, a portion (i*ibytes) of Buffer results,
+         * once those values are in their definitive place.
+         */
+        while ( data = me.read( rbytes ) ) log( 'data:', data );
     }
     , onEnd = function () {
         var me = this
